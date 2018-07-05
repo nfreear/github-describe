0e3efc78b38caa3d85d45c28dde2863ac52c1087
Nick Freear <nfreear@yahoo.co.uk>
2018-07-05 14:08:01 +0100

    Tidy `github-describe` ID etc.; start to add jQuery support

--- index.js
+++ index.js
@@ -2,52 +2,56 @@
   github-describe | © Nick Freear, 05-July-2018.
 */
 
-const REQUEST = window.superagent; // OR, require('superagent');
+// const __REQUEST = window.jQuery ? window.jQuery.fn : window.superagent; // OR, require('superagent');
+// const __GET = window.jQuery ? window.jQuery.fn.get : window.superagent.get;
+const GET_FN = ajaxGet;
+const THEN_FN = window.jQuery ? 'done' : 'then';
 
-const ELEM = document.querySelector('#id-git-describe');
-const REPO = document.querySelector('[ data-git-describe-repo ]').getAttribute('data-git-describe-repo');
+const ELEM = document.querySelector('#github-describe');
+const REPO = document.querySelector('[ data-github-describe-repo ]').getAttribute('data-github-describe-repo');
 
-const GITHUB_URL = 'https://github.com/{repo}'.replace('{repo}', REPO);
-const TAG_URL = 'https://api.github.com/repos/{repo}/tags'.replace('{repo}', REPO); // https://api.github.com/repos/nfreear/gaad-widget/tags;
-const COMP_URL = 'https://api.github.com/repos/{repo}/compare/{tag}...HEAD'.replace('{repo}', REPO); // https://api.github.com/repos/nfreear/gaad-widget/compare/3.3.0...HEAD;
-// HEAD_URL = 'https://api.github.com/repos/nfreear/gaad-widget/commits/HEAD';
+const GITHUB_URL = 'https://github.com/{repo}/commit/{sha}'.replace('{repo}', REPO);
+const TAG_URL = 'https://api.github.com/repos/{repo}/tags'.replace('{repo}', REPO);
+const COMP_URL = 'https://api.github.com/repos/{repo}/compare/{tag}...HEAD'.replace('{repo}', REPO);
 
-console.warn('git-describe', REPO, TAG_URL, ELEM);
+console.warn('github-describe:', REPO, TAG_URL, ELEM);
 
-REQUEST.get(TAG_URL).accept('json').then(function (res) {
+GET_FN(TAG_URL)/* .accept('json') */[ THEN_FN ](function (res) {
   // console.warn(res);
 
   console.assert(res.status === 200, 'HTTP status');
   console.assert(!res.clientError);
   console.assert(!res.serverError);
-  console.assert(res.header[ 'x-ratelimit-remaining' ] > 10, 'x-ratelimit-remaining');
+  console.assert(res.header[ 'x-ratelimit-remaining' ] > 2, 'x-ratelimit-remaining');
 
   const LAST_TAG = res.body[ 0 ].name;
-
-  console.warn('tag:', LAST_TAG);
-
   const COMPARE_URL = COMP_URL.replace('{tag}', LAST_TAG);
 
-  REQUEST.get(COMPARE_URL).accept('json').then(function (res) {
-    console.warn(res);
+  console.warn('GD tag:', LAST_TAG, COMPARE_URL);
+
+  GET_FN(COMPARE_URL)/* .accept('json') */[ THEN_FN ](function (res) {
+    // console.warn(res);
 
     console.assert(res.status === 200, 'HTTP status');
-    console.assert(!res.clientError);
-    console.assert(!res.serverError);
     console.assert(res.body.status === 'ahead', 'Ahead?');
     console.assert(res.body.behind_by === 0);
 
     const AHEAD_BY = res.body.ahead_by;
     const COMMIT = res.body.commits[ 0 ]; // TODO: check !!
 
-    const DESCRIBE = replaceObj('{tag}-{n}-g{sha}', { '{tag}': LAST_TAG, '{n}': AHEAD_BY, '{sha}': COMMIT.sha.substr(0, 7) });
+    const DESCRIBE = replaceObj('{tag}-{num}-g{sha}', { '{tag}': LAST_TAG, '{num}': AHEAD_BY, '{sha}': COMMIT.sha.substr(0, 7) });
     const TITLE = replaceObj('{tag} - {msg}, by {by}, on {dt}', { '{tag}': LAST_TAG, '{msg}': COMMIT.commit.message, '{by}': COMMIT.author.login, '{dt}': COMMIT.commit.author.date });
 
-    ELEM.setAttribute('href', GITHUB_URL);
+    ELEM.setAttribute('href', GITHUB_URL.replace('{sha}', COMMIT.sha));
     ELEM.setAttribute('title', TITLE);
     ELEM.innerHTML = replaceObj('<b>{repo}</b> @ <i>{desc}</i>', { '{repo}': REPO, '{desc}': DESCRIBE });
   });
 });
+
+function ajaxGet (url) {
+  const W = window;
+  return W.jQuery ? W.jQuery.getJSON(url) : W.superagent.get(url);
+}
 
 function replaceObj (str, mapObj) {
   var re = new RegExp(Object.keys(mapObj).join('|'), 'g'); // Was: "gi".
