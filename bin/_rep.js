097a17f5544f5ad9712a2dc2503bd0766a1a8c18
Nick Freear <nfreear@yahoo.co.uk>
2018-07-09 16:11:10 +0100

    Bug #1, switch from "superagent" to native browser "fetch API"
    * ... Plus, fix Travis-CI

--- bin/_rep.js
+++ bin/_rep.js
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
 
+/*!
+  CLI. Trying anonymous function wrapping. (Was: crude ES6 'const' replacement!)
+
+  @copyright © Nick Freear, 05-July-2018.
+  @license   MIT
+*/
+
+const MIN_JS = 'dist/github-describe.min.js';
 const REPLACE = require('replace');
-// const FS = require('fs');
+// const PKG = require('../package.json');
 
 console.warn('cwd:', process.cwd());
+
+REPLACE({
+  paths: [ MIN_JS ],
+  regex: '// wrap-start',
+  replacement: '(function(window){',
+  count: true
+});
+
+REPLACE({
+  paths: [ MIN_JS ],
+  regex: '// wrap-end',
+  replacement: '})(window)',
+  count: true
+});
 
 // https://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
 // FS.createReadStream('../index.js').pipe(FS.createWriteStream('../browser.js'));
@@ -15,18 +37,10 @@ console.warn('cwd:', process.cwd());
 }); */
 
 // A crude ES6 'const' to 'var' converter!
-REPLACE({
+/* REPLACE({
   paths: [ 'dist/github-describe.js' ],
   regex: 'const ',
   replacement: 'var ',
-  recursive: false,
-  silent: false
-});
-
-/* REPLACE({
-  paths: [ 'dist/browser.js' ],
-  regex: "require('superagent').get;",
-  replacement: 'ajaxGet;',
   recursive: false,
   silent: false
 }); */
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// wrap-start
+
 /*!
-  github-describe | © Nick Freear.
+  github-describe | © Nick Freear | MIT license.
 */
 
-const request = window.require ? require('superagent').get : ajaxGet;
-const THEN = window.$ ? 'done' : 'then';
+if (!('fetch' in window)) throw new Error('github-describe requires the "fetch" API.');
+
+const fetch = window.fetch;
 
 const ELEM = document.querySelector('#github-describe');
 const REPO = document.querySelector('[ data-github-describe-repo ]').getAttribute('data-github-describe-repo');
@@ -14,25 +17,56 @@ const COMP_URL = 'https://api.github.com/repos/{repo}/compare/{tag}...HEAD'.repl
 
 console.warn('github-describe:', REPO, TAG_URL, ELEM);
 
-request(TAG_URL)[ THEN ](function (data, txtStat, jqXHR) {
-  const res = jqRes(data, txtStat, jqXHR);
+var lastTag;
 
-  console.assert(res.statusCode === 200, 'HTTP stat');
-  // console.assert(!res.clientError);
-  // console.assert(res.headers[ 'x-ratelimit-remaining' ] > 2, 'x-ratelimit-remaining');
+fetch(TAG_URL)
+  .then(function (resp) {
+    console.warn('Fetch tag:', resp, resp.headers.get('x-ratelimit-remaining'));
 
-  const LAST_TAG = res.body[ 0 ].name;
-  const COMPARE_URL = COMP_URL.replace('{tag}', LAST_TAG);
+    console.assert(resp.status === 200, 'HTTP status');
+    console.assert(resp.ok, 'Ok?');
+    console.assert(resp.type === 'cors', 'CORS?');
+    console.assert(resp.bodyUsed, 'body?');
+    // console.assert(typeof resp.body === 'ReadableStream', 'body?');
 
-  console.warn('GD tag:', LAST_TAG, COMPARE_URL);
+    console.assert(resp.headers.get('x-ratelimit-remaining') > 4, 'x-ratelimit-remaining');
 
-  request(COMPARE_URL)[ THEN ](function (data, txtStat, jqXHR) {
-    const res = jqRes(data, txtStat, jqXHR);
-    const body = res.body;
+    if (!resp.ok) return Promise.reject(resp);
 
-    console.assert(res.statusCode === 200, 'HTTP stat');
+    return resp.json();
+  })
+  .then(function (body) {
+    console.assert(body.length > 0, 'tag count');
+
+    if (!body.length) return Promise.reject(new Error({ statusText: 'No git tags found', url: TAG_URL }));
+
+    const LAST_TAG = body[ 0 ].name;
+    const COMPARE_URL = COMP_URL.replace('{tag}', LAST_TAG);
+
+    console.warn('GD tag:', LAST_TAG, COMPARE_URL);
+
+    lastTag = LAST_TAG;
+
+    return fetch(COMPARE_URL);
+  })
+  .then(function (resp) {
+    console.warn('Fetch compare:', resp);
+
+    console.assert(resp.status === 200, 'HTTP status');
+    console.assert(resp.ok, 'Ok?');
+    console.assert(resp.type === 'cors', 'CORS?');
+    // console.assert(resp.bodyUsed, 'body?');
+
+    if (!resp.ok) return Promise.reject(resp);
+
+    return resp.json();
+  })
+  .then(function (body) {
     console.assert(body.status === 'ahead', 'Ahead');
     // console.assert(body.behind_by === 0);
+    console.assert(lastTag);
+
+    const LAST_TAG = lastTag;
 
     const AHEAD_BY = body.ahead_by;
     const COMMIT = body.commits[ 0 ]; // TODO: check !!
@@ -43,29 +77,8 @@ request(TAG_URL)[ THEN ](function (data, txtStat, jqXHR) {
     ELEM.setAttribute('href', GITHUB_URL.replace('{sha}', COMMIT.sha));
     ELEM.setAttribute('title', TITLE);
     ELEM.innerHTML = replaceObj('<b>{repo}</b> @ <i>{desc}</i>', { '{repo}': REPO, '{desc}': DESCRIBE });
-  });
-});
-
-function ajaxGet (url) {
-  const W = window;
-  return W.jQuery ? W.$.get(url) : W.superagent.get(url); /* .accept('json') */
-}
-
-function jqRes (data, txtStat, jqXHR) {
-  console.warn('jqRes:', data, txtStat, jqXHR);
-
-  if (window.jQuery) {
-    return {
-      // isJquery: true,
-      // headers: {}, // jqXHR.getAllResponseHeaders()
-      // ok: txtStat === 'success',
-      status: jqXHR.status,
-      statusCode: jqXHR.status,
-      body: data
-    };
-  }
-  return data;
-}
+  })
+  .catch(errResp => console.error('Fetch error:', errResp.statusText, errResp));
 
 function replaceObj (str, mapObj) {
   const RE = new RegExp(Object.keys(mapObj).join('|'), 'g'); // Was: "gi".
@@ -74,3 +87,5 @@ function replaceObj (str, mapObj) {
     return mapObj[ matched ];
   });
 }
+
+// wrap-end
